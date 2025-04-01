737908d74263c09b86f8cf7dcc8e5a372807ebaa
Matheus Rocha <matheusdasilvarocha@gmail.com>
2025-04-01 19:22:27 -0300

    fix: Removendo parte de admin

--- src/router.ts
+++ src/router.ts
@@ -1,18 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
-import Admin from './pages/Admin.vue';
-import { supabase } from './lib/supabase';
 
 const routes = [
   {
     path: '/',
     component: App,
-  },
-  {
-    path: '/admin',
-    component: Admin,
-    meta: { requiresAuth: true },
-  },
+  }
 ];
 
 const router = createRouter({
@@ -20,15 +13,5 @@ const router = createRouter({
   routes,
 });
 
-router.beforeEach(async (to, from, next) => {
-  if (to.meta.requiresAuth) {
-    const { data: { session } } = await supabase.auth.getSession();
-    if (!session) {
-      // Redirect to login if not authenticated
-      return next('/');
-    }
-  }
-  next();
-});
 
 export default router;
