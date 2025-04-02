66c9b913e6089ce0a261603e410fef32d70304a1
Matheus Rocha <matheusdasilvarocha@gmail.com>
2025-04-02 17:49:07 -0300

    chore: Corrigindo improtação do projeto

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,24 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+  ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './'),
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/assets/variables.scss" as *;`,
+        api: 'modern-compiler'
+      }
+    }
+  }
 })
